944737bf6abfd829069c3d586f65b0acae6d06aa
Behnam Sheykhe <learndlt@gmail.com>
2020-03-27 08:34:32 +0430

    add if time is <= 0 stop the timer

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ class Timer {
   };
 
   tick = () => {
-    this.timeRemaining -= 1;
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining -= 1;
+    }
   };
 
   get timeRemaining() {
